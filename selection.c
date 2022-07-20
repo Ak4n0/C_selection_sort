121328053836d1571683ed793e669badbbae3f02
Miquel Fuster <50108493+Ak4n0@users.noreply.github.com>
2022-07-20 18:41:11 +0200

    Delete selection.c

--- selection.c
+++
@@ -1,8 +0,0 @@
-#ifndef SELECCION_H__
-#define SELECCION_H__
- 
-#include <stddef.h>
- 
-void ordenar(void *base, size_t n_elem, size_t tam_elem, int (*compara)(const void* elem_1, const void* elem_2));
- 
-#endif
